e1c0216ac1f29e18a0a7c3a3d075295d395c70d1
teerapatsri-byte <teerapat.sri@sru.ac.th>
2026-09-13 19:45:13 +0700

    url

--- lib/mysql_connect.ts
+++ lib/mysql_connect.ts
@@ -1,9 +1,5 @@
 import mysql from "mysql2/promise";
 
-// Create the connection to database
 export const connect = await mysql.createConnection({
-  host: process.env.LOCALHOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  uri: process.env.DATABASE_URL,
 });
