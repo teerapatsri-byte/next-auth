3c48d688a49400fc0a866e2666402d89721ef2d5
teerapatsri-byte <teerapat.sri@sru.ac.th>
2026-09-13 21:14:10 +0700

    update sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -89,7 +89,7 @@ function Sidebar() {
               <button
                 type="button"
                 className="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                onClick={() => signOut()}
+                onClick={() => signOut({ callbackUrl: "/" })}
               >
                 <span className="flex-1 ms-3 text-left whitespace-nowrap">
                   Sign Out
